327cc1bf50a18587fd5e162c631d6cd93acfe540
hama1225 <rlaaslrb117@naver.com>
2020-11-19 17:31:16 +0900

    mybatis 세팅 완료

--- board/src/main/java/com/vod/movit/board/controller/BoardController.java
+++ board/src/main/java/com/vod/movit/board/controller/BoardController.java
@@ -13,43 +13,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
- */   
+ */
 
 @Controller
 public class BoardController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-//	@RequestMapping(value = "/", method = RequestMethod.GET)
-//	public String home(Locale locale, Model model) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
-//		
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//		
-//		String formattedDate = dateFormat.format(date);
-//		
-//		model.addAttribute("serverTime", formattedDate );
-//		
-//		return "index";
-//	}
-	
-	@RequestMapping(value="/login")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/login")
 	public String login() {
 		return "login";
 	}
-	@RequestMapping(value="/password")
+
+	@RequestMapping(value = "/password")
 	public String password() {
 		return "password";
 	}
-	@RequestMapping(value="/register")
+
+	@RequestMapping(value = "/register")
 	public String addCommunity() {
 		return "register";
 	}
-	@RequestMapping(value="/table")
+
+	@RequestMapping(value = "/table")
 	public String table() {
 		return "table";
 	}
---
+++ board/src/main/java/com/vod/movit/mybatis/BoardMapper.java
@@ -0,0 +1,16 @@
+package com.vod.movit.mybatis;
+
+import java.util.ArrayList;
+
+import com.vod.movit.board.vo.BoardVO;
+
+public interface BoardMapper {
+	ArrayList<BoardVO> getUsers();
+	/*
+	 * void insertUser(User user); void updateUser(User user); void
+	 * deleteUser(String id); User getUpdateUser(String id); User viewUser(User
+	 * user); String loginCheck(User user); User selectByEmail(String
+	 * userinfo_email); User selectById(String userinfo_id); User
+	 * selectByNickname(String userinfo_nickname);
+	 */
+}
